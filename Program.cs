e917b6a3e481c1033a448eb0ddf1fc97d3d9ad36
ERIKA LEITE PAIVA <you@example.com>
2020-11-13 10:03:41 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicio_Idade_Convite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Qual sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+            
+            if(idade >=18){
+                Console.WriteLine("Você gostria de participar do time de CS?  -Sim ou Não-");
+                string resposta = Console.ReadLine();
+                if(resposta == "Sim"){
+                    Console.WriteLine("Compareça a Secretária - Falar com a Sara");
+                }else{
+                    Console.WriteLine("Obrigada pela participação!");
+                }
+            }else{
+                Console.WriteLine("Você gostaria de participar do Time de LOL?  -Sim ou Não-");
+                string resposta = Console.ReadLine();
+                if(resposta == "Sim"){
+                    Console.WriteLine("Compareça à Secretária - Falar com Juscelino");
+                }else{
+                    Console.WriteLine("Obrigada pela participação!!");
+                }
+            }
+        }
+    }
+}
